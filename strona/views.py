ecf8b603e3b557f31a5c18c159c833850a307617
Your Name <you@example.com>
2021-10-06 22:33:05 +0100

    asd

---
+++ strona/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request): 
+    meetups = [
+        { 
+            'title' : 'A first meetup', 
+            'location' : 'New York', 
+            'slug' : 'a-first-meetup'
+        },
+        { 
+            'title' : 'A second meetup', 
+            'location' : 'Paris', 
+            'slug' : 'a-second-meetup'
+        }
+    ]
+    return render(request, 'strona/index.html', {
+        'show_meetups' : True,
+        'meetups' : meetups
+    })
+
+def meetup_details(request, meetup_slug):
+    selected_meetup = {
+            'title' : 'A first meetup',
+            'description' : 'This is firstt meetup'
+    }
+    return render(request, 'strona/meetup-details.html', {
+        'meetup_title' : selected_meetup['title'],
+        'meetup_description' : selected_meetup['description']
+    })
